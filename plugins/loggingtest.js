4fe8afb114ec0d47988d1f05e8f6c4c2b61255b7
tauraamui <tauracatamui@gmail.com>
2018-10-07 17:28:06 +0100

    work on testing plugin functions

--- plugins/loggingtest.js
+++ plugins/loggingtest.js
@@ -1,5 +1,8 @@
 
 function onLoad() {
+	for (var i = 0; i < 21; i++) {
+		console.log(i);
+	}
 	InfoLog("This is logging some information");
 	DebugLog("This is logging some debug information");
 	ErrorLog("This is logging an error");
