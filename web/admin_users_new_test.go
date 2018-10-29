54dc548389862f4baa600ccd7fb6bd0b1a2bfede
tauraamui <aplewis95@gmail.com>
2018-10-29 14:01:41 +0000

    tests for new user form input fields server side validation

--- web/admin_users_new_test.go
+++ web/admin_users_new_test.go
@@ -47,34 +47,112 @@ func TestValidatePostFormPass(t *testing.T) {
 
 func TestValidatePostFormFail(t *testing.T) {
 
-	t.Logf("%d\n", len(failTestCases))
-
 	req := httptest.NewRequest("POST", "/admin/users/root/new", nil)
 
-	for _, testCase := range failTestCases {
-		req.PostForm = testCase.vals
-		if validated, err := validatePostForm(req); err == nil || validated == true {
-			t.Errorf(testCase.onFailMessage, testCase.testName, testCase.toFail, err)
-		}
+	formValues := url.Values{}
+	formValues["authhash"] = []string{"thispassworddoesnotmatch"}
+	formValues["repeatedauthhash"] = []string{"withtheotherone"}
+
+	req.PostForm = formValues
+
+	// expected to fail due to mismatching passwords
+	if validated, err := validatePostForm(req); err == nil || validated == true {
+		t.Errorf("Test POST should have failed [MISMATCH PASSWORD], it has not: %v\n", err)
 	}
 
-	// formValues := url.Values{}
-	// formValues["authhash"] = []string{"thispassworddoesnotmatch"}
-	// formValues["repeatedauthhash"] = []string{"withtheotherone"}
+	formValues = url.Values{}
+	formValues["authhash"] = []string{"thisisatestpassword"}
+	formValues["repeatedauthhash"] = []string{"thisisatestpassword"}
+	// first name deliberately blank
+	formValues["firstname"] = []string{""}
+	formValues["lastname"] = []string{"Lastname"}
+	formValues["email"] = []string{"test@somewhere.com"}
+	formValues["username"] = []string{"testuser222"}
 
-	// req.PostForm = formValues
+	req.PostForm = formValues
 
-	// // expected to fail due to mismatching passwords
-	// if validated, err := validatePostForm(req); err == nil || validated == true {
-	// 	t.Errorf("Test POST should have failed validation, it has not: %v\n", err)
-	// }
+	// expected to fail due to blank first name
+	if validated, err := validatePostForm(req); err == nil || validated == true {
+		t.Errorf("Test POST should have failed [FIRST NAME BLANK], is has not: %v\n", err)
+	}
 
-	// formValues = url.Values{}
-	// formValues["authhash"] = []string{"thisisatestpassword"}
-	// formValues["repeatedauthhash"] = []string{"thisisatestpassword"}
-	// formValues["firstname"] = []string{""}
-	// formValues["lastname"] = []string{"Lastname"}
-	// formValues["email"] = []string{""}
-	// formValues["firstname"] = []string{""}
+	formValues = url.Values{}
+	formValues["authhash"] = []string{"thisisatestpassword"}
+	formValues["repeatedauthhash"] = []string{"thisisatestpassword"}
+	formValues["firstname"] = []string{"Firstname"}
+	// last name deliberately blank
+	formValues["lastname"] = []string{""}
+	formValues["email"] = []string{"test@somewhere.com"}
+	formValues["username"] = []string{"testuser222"}
 
+	req.PostForm = formValues
+
+	// expected to fail due to blank last name
+	if validated, err := validatePostForm(req); err == nil || validated == true {
+		t.Errorf("Test POST should have failed [LAST NAME BLANK], is has not: %v\n", err)
+	}
+
+	formValues = url.Values{}
+	formValues["authhash"] = []string{"thisisatestpassword"}
+	formValues["repeatedauthhash"] = []string{"thisisatestpassword"}
+	formValues["firstname"] = []string{"Firstname"}
+	formValues["lastname"] = []string{"Lastname"}
+	// last name name deliberately blank
+	formValues["email"] = []string{""}
+	formValues["username"] = []string{"testuser222"}
+
+	req.PostForm = formValues
+
+	// expected to fail due to blank email
+	if validated, err := validatePostForm(req); err == nil || validated == true {
+		t.Errorf("Test POST should have failed [EMAIL BLANK], is has not: %v\n", err)
+	}
+
+	formValues = url.Values{}
+	formValues["authhash"] = []string{"thisisatestpassword"}
+	formValues["repeatedauthhash"] = []string{"thisisatestpassword"}
+	formValues["firstname"] = []string{"Firstname"}
+	formValues["lastname"] = []string{"Lastname"}
+	formValues["email"] = []string{"test@somewhere.com"}
+	// username deliberately blank
+	formValues["username"] = []string{""}
+
+	req.PostForm = formValues
+
+	// expected to fail due to blank username
+	if validated, err := validatePostForm(req); err == nil || validated == true {
+		t.Errorf("Test POST should have failed [USERNAME BLANK], is has not: %v\n", err)
+	}
+
+	formValues = url.Values{}
+	formValues["authhash"] = []string{"thisisatestpassword"}
+	formValues["repeatedauthhash"] = []string{"thisisatestpassword"}
+	formValues["firstname"] = []string{"Firstname"}
+	formValues["lastname"] = []string{"Lastname"}
+	// email deliberately incorrect format
+	formValues["email"] = []string{"exampleemailplace@.com"}
+	formValues["username"] = []string{"testuser222"}
+
+	req.PostForm = formValues
+
+	// expected to fail due to incorrect email format
+	if validated, err := validatePostForm(req); err == nil || validated == true {
+		t.Errorf("Test POST should have failed [EMAIL INCORRECT FORMAT], is has not: %v\n", err)
+	}
+
+	formValues = url.Values{}
+	formValues["authhash"] = []string{"thisisatestpassword"}
+	formValues["repeatedauthhash"] = []string{"thisisatestpassword"}
+	formValues["firstname"] = []string{"Firstname"}
+	formValues["lastname"] = []string{"Lastname"}
+	formValues["email"] = []string{"test@somewhere.com"}
+	// username deliberately incorrect format
+	formValues["username"] = []string{"test*&$^user"}
+
+	req.PostForm = formValues
+
+	// expected to fail due to incorrect username format
+	if validated, err := validatePostForm(req); err == nil || validated == true {
+		t.Errorf("Test POST should have failed [USERNAME INCORRECT FORMAT], is has not: %v\n", err)
+	}
 }
