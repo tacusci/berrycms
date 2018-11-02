d65cad26bc18e7b12e419c97b72f6956b370528f
tauraamui <aplewis95@gmail.com>
2018-11-02 13:45:04 +0000

    get page confirm content match

--- web/db_pages_test.go
+++ web/db_pages_test.go
@@ -1,10 +1,19 @@
 package web
 
 import (
+	"github.com/tacusci/berrycms/db"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
+
+func init() {
+	db.Connect(db.SQLITE, "./berrycmstesting.db", "")
+	db.Wipe()
+	db.Setup()
+}
 
 func TestSavedPageGet(t *testing.T) {
 	sph := SavedPageHandler{}
@@ -17,5 +26,34 @@ func TestSavedPageGet(t *testing.T) {
 
 	if resp.StatusCode != http.StatusNotFound {
 		t.Errorf("Test get retrieved a response which is not 404...")
+	}
+
+	pt := db.PagesTable{}
+
+	pt.Insert(db.Conn, &db.Page{
+		CreatedDateTime: time.Now().Unix(),
+		Roleprotected:   false,
+		AuthorUUID:      "",
+		Title:           "Test Page",
+		Route:           "/testpage",
+		//page content is never saved as HTML but instead as QuillJS delta JSON objects
+		Content: "[{\"insert\":\"This is a test page!\\n\"}]",
+	})
+
+	req = httptest.NewRequest("GET", "/testpage", nil)
+	responseRecorder = httptest.NewRecorder()
+
+	sph.Get(responseRecorder, req)
+
+	resp = responseRecorder.Result()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Test get retrieved a respone which is not 200, STATUS: %d...", resp.StatusCode)
+	}
+
+	if bodyText, err := ioutil.ReadAll(resp.Body); err == nil {
+		if "<html><head><link rel=\"stylesheet\" href=\"/css/berry-default.css\"><link rel=\"stylesheet\" href=\"/css/font.css\"></head><p>This is a test page!</p></html>" != string(bodyText) {
+			t.Errorf("Fetched page content does not match expected content")
+		}
 	}
 }
