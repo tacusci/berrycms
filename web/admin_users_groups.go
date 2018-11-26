8cafb91d4f7774c430d59c5462ff35f6b78f887e
tauraamui <aplewis95@gmail.com>
2018-11-26 14:08:19 +0000

    revert groups initial get to selecting specific columns instead of using *

--- web/admin_users_groups.go
+++ web/admin_users_groups.go
@@ -1,7 +1,9 @@
 package web
 
 import (
+	"fmt"
 	"github.com/gobuffalo/plush"
+	"github.com/tacusci/logging"
 	"net/http"
 
 	"github.com/tacusci/berrycms/db"
@@ -14,46 +16,17 @@ type AdminUserGroupsHandler struct {
 
 func (ugh *AdminUserGroupsHandler) Get(w http.ResponseWriter, r *http.Request) {
 
-	var groups = make([]db.Group, 0)
-	var groupMemberships = make([]db.GroupMembership, 0)
+	gt := db.GroupTable{}
 
-	groupTable := db.GroupTable{}
-	groupRows, err := groupTable.Select(db.Conn, "*", "")
-
+	rows, err := gt.Select(db.Conn, "createddatetime, uuid, title", "")
 	if err != nil {
 		Error(w, err)
 	}
 
-	defer groupRows.Close()
-
-	for groupRows.Next() {
-		g := db.Group{}
-
-		groupRows.Scan(&g.CreatedDateTime, &g.UUID, &g.Title)
-		groups = append(groups, g)
-		groupMembershipTable := db.GroupMembershipTable{}
-		groupMembershipRows, err := groupMembershipTable.Select(db.Conn, "*", "groupuuid = "+g.UUID)
-
-		if err != nil {
-			Error(w, err)
-		}
-
-		defer groupMembershipRows.Close()
-
-		for groupMembershipRows.Next() {
-			gm := db.GroupMembership{}
-
-			groupMembershipRows.Scan(&gm.CreatedDateTime, &gm.UUID, &gm.GroupUUID, &gm.UserUUID)
-			groupMemberships = append(groupMemberships, gm)
-			userTable := db.UsersTable{}
-			userRows, err := userTable.Select(db.Conn, "*", "uuid = "+gm.UserUUID)
-
-			if err != nil {
-				Error(w, err)
-			}
-
-			defer userRows.Close()
-		}
+	for rows.Next() {
+		group := db.Group{}
+		rows.Scan(&group.CreatedDateTime, &group.UUID, &group.Title)
+		logging.Debug(fmt.Sprintf("Loaded group {CT: %s, UUID: %s, Title: %s}", UnixToTimeString(group.CreatedDateTime), group.UUID, group.Title))
 	}
 
 	pctx := plush.NewContext()
