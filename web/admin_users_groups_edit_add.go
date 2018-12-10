7fb6a55800d209271b9d073de2c03a6a12571f49
tauraamui <aplewis95@gmail.com>
2018-12-10 17:04:47 +0000

    read user records to add

--- web/admin_users_groups_edit_add.go
+++ web/admin_users_groups_edit_add.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/tacusci/berrycms/db"
 	"net/http"
 )
@@ -15,5 +16,18 @@ func (augeah *AdminUserGroupsEditAddHandler) Get(w http.ResponseWriter, r *http.
 	vars := mux.Vars(r)
 
 	ut := db.UsersTable{}
-	rows, err := ut.Select(db.Conn, "uuid, username")
+	rows, err := ut.Select(db.Conn, "uuid, username", vars["uuid"])
+
+	if err != nil {
+		Error(w, err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		user := db.User{}
+		rows.Scan(&user.UUID, &user.Username)
+
+		users = append(users, user)
+	}
 }
