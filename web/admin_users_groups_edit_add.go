03ce252666a929e9b50991f232b99ee6764169b7
tauraamui <aplewis95@gmail.com>
2018-12-14 17:46:49 +0000

    add users posted to group

--- web/admin_users_groups_edit_add.go
+++ web/admin_users_groups_edit_add.go
@@ -1,6 +1,11 @@
 package web
 
 import (
+	"errors"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/tacusci/berrycms/db"
+	"github.com/tacusci/logging"
 	"net/http"
 )
 
@@ -16,6 +21,63 @@ func (augeah *AdminUserGroupsEditAddHandler) Get(w http.ResponseWriter, r *http.
 //Post handles post requests to URI
 func (augeah *AdminUserGroupsEditAddHandler) Post(w http.ResponseWriter, r *http.Request) {
 
+	vars := mux.Vars(r)
+	groupUUID := vars["uuid"]
+
+	if groupUUID == "" {
+		Error(w, errors.New("Missing group UUID"))
+		return
+	}
+
+	var redirectURI = "/admin/users/groups/edit/" + groupUUID
+
+	if augeah.Router.AdminHidden {
+		redirectURI = fmt.Sprintf("/%s", augeah.Router.AdminHiddenPassword) + redirectURI
+	}
+
+	defer http.Redirect(w, r, redirectURI, http.StatusFound)
+
+	err := r.ParseForm()
+
+	if err != nil {
+		Error(w, err)
+		return
+	}
+
+	ut := db.UsersTable{}
+	gmt := db.GroupMembershipTable{}
+	gt := db.GroupTable{}
+	amw := AuthMiddleware{}
+
+	loggedInUser, err := amw.LoggedInUser(r)
+
+	if loggedInUser != nil {
+
+		groupTitle := ""
+		rows, err := gt.Select(db.Conn, "title", fmt.Sprintf("uuid = '%s'", groupUUID))
+		if err != nil {
+			Error(w, err)
+			return
+		}
+
+		for rows.Next() {
+			rows.Scan(&groupTitle)
+		}
+
+		for _, v := range r.PostForm {
+			userToAdd, err := ut.SelectByUUID(db.Conn, v[0])
+			if err != nil {
+				logging.Error(err.Error())
+				continue
+			}
+
+			if userToAdd == nil {
+				continue
+			}
+
+			gmt.AddUserToGroup(db.Conn, userToAdd, groupTitle)
+		}
+	}
 }
 
 //Route get URI route for handler
