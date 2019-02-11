7ea0bbf9b09f9b2843cf3a7f40e08e7b32e7a50f
tauraamui <aplewis95@gmail.com>
2019-02-11 10:48:27 +0000

    add routes to test loading bytes data as image

--- plugins/app.js
+++ plugins/app.js
@@ -1,6 +1,6 @@
 
 function onGetRender(uri) {
-    if (uri === "/login") {
+    if (uri === "/main.go") {
         var pageAndResult = session.Get("login_page");
         if (pageAndResult[1]) {
             document.SetHtml(pageAndResult[0]);
@@ -11,7 +11,7 @@ function onGetRender(uri) {
 function onPostRecieve(uri, data) {}
 
 //this list of routes gets mapped on plugin load, before main() is called
-var routesToRegister = ["/login"];
+var routesToRegister = ["/main.go", "/images/{imgfile}"];
 
 function main() {
     var loginPage = files.Read("./main.go");
