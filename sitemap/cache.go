90d6ef990ebbec3edbcc99e4d1f03e0364fc03a9
tauraamui <aplewis95@gmail.com>
2019-02-15 00:31:38 +0000

    start work on sitemap cache init generation

--- sitemap/cache.go
+++ sitemap/cache.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bytes"
 	"errors"
+	"fmt"
+
+	"github.com/tacusci/berrycms/db"
 )
 
 var cache *bytes.Buffer
@@ -13,13 +16,43 @@ func Add(val *[]byte) error {
 	}
 }
 
-func Del(val *[]byte) error {}
+func Del(val *[]byte) error {
+	return nil
+}
 
-func Generate(adminPagesDisabled bool) error {}
+func Generate(adminPagesDisabled bool) error {
+	Reset()
+	_, err := cache.WriteString("<?xml version\"1.0\" encoding\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n\t")
+	if err != nil {
+		return err
+	}
 
-func CacheExists() bool {}
+	pt := db.PagesTable{}
+	rows, err := pt.Select(db.Conn, "route", "roleprotected = '0'")
 
-func CacheBytes() []byte {}
+	if err != nil {
+		return err
+	}
+
+	var pageRouteToAdd string
+
+	for rows.Next() {
+		err := rows.Scan(&pageRouteToAdd)
+		if err != nil {
+			return err
+		}
+
+		_, err := cache.WriteString(fmt.Sprintf("<url>%s"))
+	}
+}
+
+func CacheExists() bool {
+	return cache != nil
+}
+
+func CacheBytes() []byte {
+	return cache.Bytes()
+}
 
 func Reset() {
 	//we don't want to allocate memory each reset
