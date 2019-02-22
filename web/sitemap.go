383496e29a466086b8e4c54d9ca8cbea1ae9ea48
tauraamui <aplewis95@gmail.com>
2019-02-22 09:02:46 +0000

    handle generate on first get for sitemap.xml

--- web/sitemap.go
+++ web/sitemap.go
@@ -1,9 +1,11 @@
 package web
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/tacusci/berrycms/sitemap"
+	"github.com/tacusci/logging"
 )
 
 type SitemapHandler struct {
@@ -12,10 +14,20 @@ type SitemapHandler struct {
 }
 
 func (sh *SitemapHandler) Get(w http.ResponseWriter, r *http.Request) {
-	//if the sitemap.xml file cache hasn't been created then technically there is no sitemap page
-	if !sitemap.CacheExists() {
+	//if the sitemap.xml has been disabled, don't continue
+	if sh.Router.NoSitemap {
 		fourOhFour(w, r)
 		return
+	}
+
+	//if the sitemap.xml page hasn't been visited before cache won't have been generated
+	if !sitemap.CacheExists() {
+		logging.Debug(fmt.Sprintf("Sitemap.xml cache doesn't exist yet, creating it with URL hostname: %s", r.Host))
+		//creates a sitemap string and loads into in-memory cache
+		err := sitemap.Generate(r.URL.Scheme, r.Host)
+		if err != nil {
+			logging.Error(err.Error())
+		}
 	}
 
 	w.Write(sitemap.CacheBytes())
